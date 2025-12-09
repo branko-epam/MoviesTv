3f15283067eeccb36a5bc82af151910999279aef
branko-epam <branko_popovic1@epam.com>
2025-12-09 11:16:14 +0100

    initial

---
+++ MoviesTv/MoviesTv/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoviesTv
+//
+//  Created by Branko Popovic1 on 9. 12. 2025..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoviesTv/MoviesTv/MoviesTvApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesTvApp.swift
+//  MoviesTv
+//
+//  Created by Branko Popovic1 on 9. 12. 2025..
+//
+
+import SwiftUI
+
+@main
+struct MoviesTvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
